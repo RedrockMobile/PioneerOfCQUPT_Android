ef3199200e17b379d948fe2a6dcf4461c844434d
78848d676612 <443657547@qq.com>
2016-06-15 22:32:07 +0800

    person

--- app/src/main/java/com/mredrock/cypioneer/ui/fragment/bottom_fragment/PersonalCenterFragment.java
+++ app/src/main/java/com/mredrock/cypioneer/ui/fragment/bottom_fragment/PersonalCenterFragment.java
@@ -1,12 +1,11 @@
 package com.mredrock.cypioneer.ui.fragment.bottom_fragment;
 
 import android.app.Dialog;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
