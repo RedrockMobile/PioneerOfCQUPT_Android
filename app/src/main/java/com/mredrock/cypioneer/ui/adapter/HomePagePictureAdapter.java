6904e7c1ccb3502d21cd8a49ac0530b01ccd22ff
xushuzhan <391830728@qq.com>
2016-06-18 21:33:10 +0800

    使用了glide添加轮播图图片

--- app/src/main/java/com/mredrock/cypioneer/ui/adapter/HomePagePictureAdapter.java
+++ app/src/main/java/com/mredrock/cypioneer/ui/adapter/HomePagePictureAdapter.java
@@ -1,12 +1,16 @@
 package com.mredrock.cypioneer.ui.adapter;
 
 import android.graphics.Bitmap;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.jude.rollviewpager.adapter.StaticPagerAdapter;
 import com.mredrock.cypioneer.R;
+import com.mredrock.cypioneer.model.bean.PhotoBean;
 
 import java.util.ArrayList;
 
@@ -15,21 +19,20 @@ import java.util.ArrayList;
  * Created by xushuzhan on 2016/6/16.
  */
 public class HomePagePictureAdapter extends StaticPagerAdapter {
-    private int[] imgs = {
-            R.drawable.a,
-            R.drawable.b,
-            R.drawable.c,
-
-    };
-    ArrayList<Bitmap> pictures = new ArrayList<>();
-    public  HomePagePictureAdapter(ArrayList<Bitmap> pictures){
-        this.pictures = pictures;
+    Fragment fragment;//待传入的的fragment
+    ArrayList<PhotoBean.DataBean> carouselFigures;//轮播图的图片实体类
+    public  HomePagePictureAdapter(Fragment fragment, ArrayList<PhotoBean.DataBean> carouselFigures){
+        this.fragment = fragment;
+        this.carouselFigures=carouselFigures;
     }
     @Override
     public View getView(ViewGroup container, int position) {
+        Log.d("HomePagePictureAdapter", "getView: position"+carouselFigures.get(position).getImgurl());
         ImageView view = new ImageView(container.getContext());
-       // view.setImageBitmap(pictures.get(position));
-        view.setImageResource(imgs[position]);
+        Glide.with(fragment)
+                .load("http://202.202.43.42/lxyz/Public/uploads/2016-06-07/575676790d7db.jpg")
+                .into(view);
+
         view.setScaleType(ImageView.ScaleType.CENTER_CROP);
         view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         return view;
@@ -37,7 +40,6 @@ public class HomePagePictureAdapter extends StaticPagerAdapter {
 
     @Override
     public int getCount() {
-        return imgs.length;
-        //return pictures.size();
+        return carouselFigures.size();
     }
 }
