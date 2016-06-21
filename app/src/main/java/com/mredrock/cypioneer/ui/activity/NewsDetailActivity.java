810fc5ba25f1e045592fd52811ae57c8cde1e6f8
xushuzhan <391830728@qq.com>
2016-06-21 18:17:28 +0800

    更新了主页的布局，完善了资讯列表的跳转功能

--- app/src/main/java/com/mredrock/cypioneer/ui/activity/NewsDetailActivity.java
+++ app/src/main/java/com/mredrock/cypioneer/ui/activity/NewsDetailActivity.java
@@ -3,6 +3,8 @@ package com.mredrock.cypioneer.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,23 +16,54 @@ import com.mredrock.cypioneer.net.HttpMethods;
 import rx.Subscriber;
 
 public class NewsDetailActivity extends AppCompatActivity {
-    TextView tv_title;
-    TextView tv_sub_title;
+    TextView tvTitle;
+    TextView tvSubTitle;
     String newsId;
     String newsDtail;
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+        tvTitle = (TextView) findViewById(R.id.tv_title_toolbar);
+        tvSubTitle = (TextView) findViewById(R.id.tv_sub_title_toolbar);
 
-        tv_title = (TextView) findViewById(R.id.tv_title_toolbar);
-        tv_sub_title = (TextView) findViewById(R.id.tv_sub_title_toolbar);
-
-        tv_sub_title.setText("");
+        tvSubTitle.setText("");
 
         Intent intent  = getIntent();
         newsId = intent.getStringExtra("news_id");
-        Toast.makeText(NewsDetailActivity.this, newsId, Toast.LENGTH_SHORT).show();
+
+        webView = (WebView) findViewById(R.id.web_view_news_dtail);
+
+        getNews();
     }
+
+    private void getNews() {
+        Subscriber<NewsDetailBean> subscriber = new Subscriber<NewsDetailBean>() {
+
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(NewsDetailActivity.this, "网络好像不太顺畅...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(NewsDetailBean newsDetailBean) {
+                newsDtail=newsDetailBean.getData().getContent();
+                tvTitle.setText(newsDetailBean.getData().getTitle());
+                webView.loadDataWithBaseURL(null, newsDetailBean.getData().getTime()+newsDtail, "text/html", "utf-8", null);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.setWebChromeClient(new WebChromeClient());
+
+            }
+        };
+        HttpMethods.getInstance().getNewsDetail(subscriber,Integer.parseInt(newsId));
+    }
+
 
 }
